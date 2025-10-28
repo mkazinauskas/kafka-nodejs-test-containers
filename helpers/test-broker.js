920734488c536a11ea0a2d84df5ea45c22eaf3e8
Modestas Kažinauskas <m.kazinauskas@gmail.com>
2025-10-28 23:28:15 +0200

    a

---
+++ helpers/test-broker.js
@@ -0,0 +1,33 @@
+import { Kafka, logLevel } from "kafkajs";
+
+const GROUP_ID = "test-group";
+const TOPIC_NAME = "test-topic";
+const TEST_MESSAGE = "test message";
+
+export default async function testBroker(brokers) {
+  const kafka = new Kafka({ logLevel: logLevel.NOTHING, brokers });
+
+  const producer = kafka.producer();
+  await producer.connect();
+
+  const consumer = kafka.consumer({ groupId: GROUP_ID });
+  await consumer.connect();
+
+  await producer.send({ topic: TOPIC_NAME, messages: [{ value: TEST_MESSAGE }] });
+  await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
+
+  const consumedMessage = await new Promise((resolve) =>
+    consumer.run({
+      eachMessage: async ({ message }) => resolve(message.value?.toString()),
+    })
+  );
+
+  if (consumedMessage !== TEST_MESSAGE) {
+    throw new Error("Consumed message is undefined");
+  }
+
+  await consumer.disconnect();
+  await producer.disconnect();
+
+  console.log("Successfully connected to Kafka broker:", brokers);
+}
